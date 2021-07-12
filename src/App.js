59c4621dca307f21d4e4d43aec469a8492be7d79
ardihp <ardihp8@gmail.com>
2021-07-12 19:13:28 +0700

    Handson Module 2 Session 1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form className="search">
+        <input type="text" />
+        <input type="submit" value="search" />
+      </form>
+      <div className="gambar">
+        <img
+          src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"
+          alt="gambar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
